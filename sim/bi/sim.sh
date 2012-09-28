1387471e228e1397d0ca5fab1d34d43961ce2140
Julian Grahsl <julian@newt.localdomain.com>
2012-09-28 11:22:48 +0200

    sim fixed under linux. compile pre and post for windows sim calls those

--- sim/bi/sim.sh
+++ sim/bi/sim.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-VHD="txt_util.vhd cam_pkg.vhd null.vhd pipe_head.vhd sim_feed.vhd sim_sink.vhd romdata.vhd rom.vhd bi.vhd"
-
 export PATH=$PATH:/opt/model6.4/linux
 export LM_LICENSE_FILE=/opt/model6.4/lic.dat
 
@@ -9,49 +7,10 @@ STIM=$1
 SRCDIR="../.."
 SIMBASEDIR=".."
 
-TB=tb.vhd
 
-#CLEAN
-rm -f sim.dat sim.out output.tiff 
+./pre.sh $STIM
+./compile.sh $STIM
 
-#COMPILE
-rm -fR work
-vlib work
-for i in $VHD; do
-    vcom -93 -work work $SRCDIR/$i  
-    if [ $? -ne 0 ]; then
-        exit;
-    fi
-done
+vsim -c -do $SIMBASEDIR/sim_batch.do
 
-#COMILE TB AND PKG
-vcom -93 -work work $STIM/sim_pkg.vhd && vcom -93 -work work $TB
-if [ $? -ne 0 ]; then
-    exit;
-fi
-
-#INPUT DATA
-$SIMBASEDIR/tiff2dat.py $STIM/input.tiff > sim.dat
-cat sim.dat sim.dat sim.dat sim.dat sim.dat sim.dat sim.dat sim.dat > sim8.dat
-cp sim8.dat sim.dat
-
-vsim -do $SIMBASEDIR/sim.do
-
-cat sim.out | $SIMBASEDIR/dat2tiff.py $STIM/output.tiff
-cat $STIM/sim.out | $SIMBASEDIR/dat2tiff.py $STIM/expect.tiff
-
-echo "Difference:"
-diff sim.out $STIM/sim.out
-
-#CLEANUP
-mv transcript $STIM
-mv sim.out $STIM/last.out
-rm sim8.dat sim.dat vsim.wlf
-
-#convert $STIM -scale 256x256 i.tiff
-#convert output.tiff -scale 256x256 o.tiff
-#rm output.tiff
-
-#ghdl -m --workdir=simu --work=work tb 
-#./tb --stop-time=100us 
-
+./post.sh $STIM
--- sim/sim_batch.do
+++ sim/sim_batch.do
@@ -5,7 +5,7 @@ vsim -novopt -t ps tb
 nolog -r /*
 set NumericStdNoWarnings 1
 set StdArithNoWarnings 1
-when -label end_of_simulation {/tb/finish == '1'} {echo "End of simulation" ; stop ; quit -f }
+when -label end_of_simulation {/tb/finish == '1'} {echo "Successfully end of simulation" ; stop ; quit -f}
 when -label enable_StdWarn {/tb/rst == '0'} { set StdArithNoWarnings 0 ; set NumericStdNoWarning 0 }
 transcript file "transcript"
 
