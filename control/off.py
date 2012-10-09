90a7af9e0e6866a3662fcbbf5d9a3c4ea043d809
Julian Grahsl <julian@newt.localdomain.com>
2012-10-09 23:40:38 +0200

    Added calibration suplements.

---
+++ control/off.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+
+import sys
+x = 0
+y = 0
+c = sys.stdin.readline().strip()
+while c != "":
+
+    l  = c.split()
+    try:
+        for i in l:
+            if sys.argv[1] == "x":
+                off = x - float(i)
+                print off
+
+            if sys.argv[1] == "y":
+                off = y - float(i)
+                print off
+
+            if x < (320-1):
+                x = x + 1
+            else:
+                x = 0
+
+                if y < (240-1):
+                    y = y + 1
+                else:
+                    y = 0
+
+    except Exception,e:
+        z=0
+
+    c = sys.stdin.readline().strip()
+
