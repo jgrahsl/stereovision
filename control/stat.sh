6cba7d6b5a96dab4f3fd8fc1795c43234418f108
Julian Grahsl <julian@newt.localdomain.com>
2012-10-17 11:25:41 +0200

    Added statistics scriptwq

---
+++ control/stat.sh
@@ -0,0 +1,13 @@
+
+cat mx1.xml | ./off.py  x  |sort -n |head -n1
+cat mx1.xml | ./off.py  x  |sort -n |tail -n1
+
+cat my1.xml | ./off.py  y  |sort -n |head -n1
+cat my1.xml | ./off.py  y  |sort -n |tail -n1
+
+cat mx2.xml | ./off.py  x  |sort -n |head -n1
+cat mx2.xml | ./off.py  x  |sort -n |tail -n1
+
+cat my2.xml | ./off.py  y  |sort -n |head -n1
+cat my2.xml | ./off.py  y  |sort -n |tail -n1
+
