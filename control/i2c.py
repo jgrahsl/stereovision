2a9ae5859a01742e6ca8f0398c17e4d7b15dfa06
Julian Grahsl <julian@newt.localdomain.com>
2012-10-25 14:34:40 +0200

    not working

--- control/i2c.py
+++ control/i2c.py
@@ -6,30 +6,60 @@ from PIL import Image
 vp = "1443:0007"
 handle = None
 
-def send_byte(chan,val):
-    global handle
-    ba = bytearray(1)
-    ba[0] = val
-#    ba[1] = val
-    flWriteChannel(handle, 1000, chan,ba)
 
-def recv_byte(chan):
-    global handle
-    return flReadChannel(handle, 1000, chan,1)
+#                IRD & x"30001580", -- Chip version. Default 0x1580
 
-def set_pipe(n):
-    send_byte(0x60,n)
 
-def set_enable(adr,en):
-    set_reg(adr,0x61,en)
+init = ["33860501", "MCU Reset",
+"33860500", "MCU Release from reset",
+"32140D85", "Slew rate control, PCLK 5, D 5",
+"341E8F0B", "PLL control; bypassed, powered down",
+"341C0250", "PLL dividers; M=80,N=2,fMCLK=fCLKIN*M/(N+1)/8=80MHz",
+"341E8F09", "PLL control; Power-up PLL; wait 1ms after this!",
+"341E8F08", "PLL control; Turn off bypass",
+"32020008", "Standby control; Wake up",
+"338C2797", "Output format; Context B shadow",
+"33900030", "RGB with BT656 codes",
+"338C272F", "Sensor Row Start Context B",
+"33900004", "4",
+"338C2733", "Sensor Row End Context B",
+"339004BB", "1211",
+"338C2731", "Sensor Column Start Context B",
+"33900004", "4",
+"338C2735", "Sensor Column End Context B",
+"3390064B", "1611",
+"338C2707", "Output width; Context B",
+"33900140", "1600",
+"338C2709", "Output height; Context B",
+"339000F0", "1200",
+"338C275F", "Crop X0; Context B",
+"33900000", "0",
+"338C2763", "Crop Y0; Context B",
+"33900000", "0",
+"338C2761", "Crop X1; Context B",
+"33900640", "1600",
+"338C2765", "Crop Y1; Context B",
+"339004B0", "1200",
+"338C2741", "Sensor_Fine_IT_min B",
+"33900169", "361               ",
+"338CA120", "Capture mode options",
+"33900002", "Turn on AE, Video",
+"338CA137", "Capture mode options",
+"33900000", "AE Manual mde",
+"338CA223", "Capture mode options",
+"33900000", "Integration time                                         ",
+"338CA103", "Refresh Sequencer Mode",
+"33900002", "Capture"]
 
-def set_reg(adr,reg,en):
-    set_pipe(adr)
-    send_byte(reg,en)
+init2 = ["301A02CC","reset/output control; parallel enable, drive pins, start streaming"]
 
-def get_reg(adr,reg):
-    set_pipe(adr)
-    return recv_byte(reg)
+         #       IRD & x"33900000", -- Read until sequencer in mode 0 (run)
+                
+
+
+
+
+
 
 try:
     try:
@@ -46,7 +76,7 @@ try:
         print "Attempting to open connection to FPGALink device %s ..." % vp
         handle = flOpen(vp)
 
-        set_reg(0,0,0)
+        flWriteChannel(handle, 1000, 0x21,0)
 
 except FLException, ex:
 #    print str(ex)
@@ -60,7 +90,7 @@ print "-"
 #if sys.argv[1] == "pic":
 #    readpic()
 
-def tx():
+def tx(ba):
     flWriteChannel(handle, 1000, 0x21,ba)
 
 def tx2():
@@ -78,17 +108,24 @@ def rx():
         else:
             print "ok"
 
-ba = bytearray(5)
-ba[0] = 0x78
-ba[1] = 0x30
-ba[2] = 0x1A   
-ba[3] = 0x02
-ba[4] = 0xCC
+for i in range(len(init)/2):
+    ba = bytearray(5)
+    ba[0] = 0x78
+    ba[1] = int(init[2*i][0:2],16)
+    ba[2] = int(init[2*i][2:4],16)
+    ba[3] = int(init[2*i][4:6],16)
+    ba[4] = int(init[2*i][6:8],16)  
+    print ba
+    tx(ba)
+#    rx()
+    sys.stdin.readline()
 
+flClose(handle)
+exit()
 
-tx()
+#tx()
 #tx2()
-rx()
+#rx()
 #f.close()
 flClose(handle)
 exit()
